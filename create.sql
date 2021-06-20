adab36af26adaa0fa9cc6b6e11166e26b39e77dd
Karthik Sreenivasan <krtsrn01@gmail.com>
2021-06-20 13:58:39 +0530

    change

--- create.sql
+++ create.sql
@@ -1,9 +1,21 @@
-PRAGMA foreign_keys = ON
+CREATE TABLE publisher(
+        id integer PRIMARY KEY,
+        name text,
+        country text
+);
 
+CREATE TABLE books(
+        id integer PRIMARY KEY,
+        title text,
+        publisher integer references publisher(id)
+);
 
-CREATE TABLE publisher (id integer  PRIMARY KEY ,name text,country text);
-CREATE TABLE books (id integer PRIMARY KEY  , title text ,publisher INTEGER REFERENCES publisher(id));
-CREATE TABLE subjects(id integer PRIMARY KEY, name text);
-CREATE TABLE books_subjects(book INTEGER references books(id), subject integer references subjectS(id));
+CREATE TABLE subjects(
+        id integer PRIMARY KEY,
+        name text
+);
 
-
+CREATE TABLE books_subjects(
+        book integer references books(id),
+        subject integer references subjects(id)
+);
